d0e5de3997d19bf46ae258e510cfd4adddd40085
Jiahao Guo <100775189+autJiahao@users.noreply.github.com>
2023-05-17 02:16:43 +1200

    Delete some notes

--- Demo05.py
+++ Demo05.py
@@ -17,10 +17,10 @@ from deep_sort.utils.parser import get_config
 from models.experimental import attempt_load
 
 FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0]  # YOLOv5 root directory 根目录
+ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
-ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # 绝对路径转换为相对路径
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))
 
 from utils.datasets import LoadImages
 from utils.general import (check_img_size, cv2, xyxy2xywh, non_max_suppression, scale_coords)
@@ -30,7 +30,7 @@ from deep_sort.deep_sort import DeepSort
 
 
 class Yolov5Thread(QThread):
-    send_img = pyqtSignal(np.ndarray)  # pyqtSignal是pyqt里非常实用的一个接口，是PyQt5 提供的自定义信号类；
+    send_img = pyqtSignal(np.ndarray)
 
     def __init__(self):
         super(Yolov5Thread, self).__init__()
@@ -39,7 +39,7 @@ class Yolov5Thread(QThread):
         self.source = ''
         self.conf = 0.3
         self.iou = 0.5
-        self.jump_out = False  # 暂停视频
+        self.jump_out = False
         self.is_continue = True  # continue/pause
 
     def run(self,
@@ -57,26 +57,24 @@ class Yolov5Thread(QThread):
             half=False,  # use FP16 half-precision inference
             ):
 
-        # try-catch是用来捕捉异常, 并进行安全输出。
         try:
             print("Yolo start")
-            device = select_device(device)  # 获取设备, CPU还是GPU
+            device = select_device(device)
             half &= device.type != 'cpu'  # half precision only supported on CUDA
 
             # Load model
             model = attempt_load(self.weights, map_location=device)  # load FP32 model
             stride = int(model.stride.max())  # model stride
-            imgsz = check_img_size(imgsz, s=stride)  # check image size, 能否整除32, 调整图片
+            imgsz = check_img_size(imgsz, s=stride)  # check image size
             if half:
                 model.half()  # to FP16
 
-            # 加载图片和视频
             dataset = LoadImages(self.source, img_size=imgsz, stride=stride)
-            # COCO数据集：人, 车
+            # COCO
             names = model.module.names if hasattr(model, 'module') else model.names  # get class names
             dataset = iter(dataset)
 
-            while True:  # 循环每一张图片
+            while True:
                 if self.jump_out:
                     break
 
@@ -98,7 +96,7 @@ class Yolov5Thread(QThread):
                     self.current_weight = self.weights
 
                 if self.is_continue:
-                    path, img, im0s, vid_cap = next(dataset)  # 读取文件路径, 图片, 视频
+                    path, img, im0s, vid_cap = next(dataset)
                     img = torch.from_numpy(img).to(device)  # 转化Tensor格式
                     img = img.half() if half else img.float()  # uint8 to fp16/32
                     img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -107,59 +105,20 @@ class Yolov5Thread(QThread):
                     if img.ndimension() == 3:
                         img = img.unsqueeze(0)
 
-                    """
-                        pred.shape=(1, num_boxes, 5+num_class)
-                        h,w为传入网络图片的长和宽,注意dataset在检测时使用了矩形推理,所以这里h不一定等于w
-                        num_boxes = h/32 * w/32 + h/16 * w/16 + h/8 * w/8
-                        pred[..., 0:4]为预测框坐标=预测框坐标为xywh(中心点+宽长)格式
-                        pred[..., 4]为objectness置信度
-                        pred[..., 5:-1]为分类结果
-                    """
-
                     pred = model(img, augment=augment)[0]
 
-                    """
-                      pred: 网络的输出结果
-                      conf:置信度阈值
-                      iou:iou阈值
-                      classes: 是否只保留特定的类别
-                      agnostic_nms: 进行nms是否也去除不同类别之间的框
-                      max-det: 保留的最大检测框数量
-                      ---NMS, 预测框格式: xywh(中心点+长宽)-->xyxy(左上角右下角)
-                      pred是一个列表list[torch.tensor], 长度为batch_size
-                      每一个torch.tensor的shape为(num_boxes, 6), 内容为box + conf + cls
-                    """
-
-                    # Apply NMS, 非极大抑直
+                    # Apply NMS
                     pred = non_max_suppression(pred, self.conf, self.iou, classes, agnostic_nms, max_det=max_det)
 
-                    # Process detections, 对每一张图片进行处理
+                    # Process detections
                     for i, det in enumerate(pred):  # detections per image
-                        im0 = im0s.copy()  # 是否裁剪检测框
-                        '''
-                        这段代码是使用一个名为Annotator的对象来标注一张图片（im0），
-                        并将线条宽度设置为line_thickness，
-                        同时在注释示例中提供了一个字符串(names)。
-                        '''
+                        im0 = im0s.copy()
+
                         annotator = Annotator(im0, line_width=line_thickness, example=str(names))
 
                         if len(det):
                             # Rescale boxes from img_size to im0 size 将预测信息映射到原图
                             det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-
-                            '''
-                            该代码使用 reversed() 函数以相反的顺序遍历 detections det 列表。
-                            对于每次检测，代码提取三个值：xyxy、conf 和 cls。
-                            xyxy 可能是一组四个坐标，用于定义图像中对象周围的边界框。
-                            conf 是与检测相关的置信度分数，它表示对象属于检测到的类别的可能性有多大。
-                            cls 是检测到的对象的预测类标签。
-                            该代码使用 int() 函数将 cls 转换为整数。
-                            根据 hide_labels 和 hide_conf 的值，代码为检测到的对象创建标签或将标签设置为 None。
-                            如果 hide_labels 为 False，则代码使用名称列表查找与 c 对应的类标签的名称。
-                            如果 hide_conf 为 False，则代码使用 f 字符串将置信度分数附加到标签。
-                            然后代码调用函数 annotator.box_label() 在检测到的对象周围绘制一个边界框，并使用标签变量对其进行标记。 colors() 函数用于根据类标签为边界框选择颜色。
-                            总体而言，此代码可能会可视化图像中的对象检测，并可选择显示/隐藏标签和置信度分数。
-                            '''
 
                             for *xyxy, conf, cls in reversed(det):
                                 lbl = names[int(cls)]
@@ -177,7 +136,7 @@ class Yolov5Thread(QThread):
 
 
 class DeepsortThread(QThread):
-    send_img2 = pyqtSignal(np.ndarray)  # pyqtSignal是pyqt里非常实用的一个接口，是PyQt5 提供的自定义信号类；
+    send_img2 = pyqtSignal(np.ndarray)
 
     def __init__(self):
         super(DeepsortThread, self).__init__()
@@ -186,9 +145,9 @@ class DeepsortThread(QThread):
         self.source = ''
         self.conf = 0.3
         self.iou = 0.5
-        self.jump_out = False  # 暂停视频
-        self.is_continue = True  # continue/pause
-        self.config_deepsort = r'D:\AUT\COMP702 笔记\Demo\deep_sort\configs\deep_sort.yaml'
+        self.jump_out = False
+        self.is_continue = True  # continue/pause'
+        self.config_deepsort = '../deep_sort/configs/deep_sort.yaml'
 
     def run(self,
             imgsz=640,  # inference size (pixels)
@@ -223,18 +182,18 @@ class DeepsortThread(QThread):
             model = attempt_load(self.weights, map_location=device)  # load FP32 model
 
             stride = int(model.stride.max())  # model stride
-            imgsz = check_img_size(imgsz, s=stride)  # check image size, 能否整除32, 调整图片
+            imgsz = check_img_size(imgsz, s=stride)  # check image size
 
             if half:
                 model.half()  # to FP16
 
-            # 加载图片和视频
+            # load videos
             dataset = LoadImages(self.source, img_size=imgsz, stride=stride)
             dataset = iter(dataset)
             prev_trackers = {}
             trajectories = {}
 
-            # 循环每一张图片
+            # while
             while True:
                 if self.jump_out:
                     break
@@ -257,17 +216,17 @@ class DeepsortThread(QThread):
                     self.current_weight = self.weights
 
                 if self.is_continue:
-                    path, img, im0s, vid_cap = next(dataset)  # 读取文件路径, 图片, 视频
-                    img = torch.from_numpy(img).to(device)  # 转化Tensor格式
+                    path, img, im0s, vid_cap = next(dataset)
+                    img = torch.from_numpy(img).to(device)
                     img = img.half() if half else img.float()  # uint8 to fp16/32
-                    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+                    img /= 255.0
                     if img.ndimension() == 3:
                         img = img.unsqueeze(0)
 
                     # Inference
                     pred = model(img, augment=augment, visualize=visualize)[0]
 
-                    # Apply NMS, 非极大抑直, 进行筛选
+                    # Apply NMS
                     pred = non_max_suppression(pred, self.conf, self.iou, classes, agnostic_nms)
 
                     #  Process detections
@@ -279,27 +238,13 @@ class DeepsortThread(QThread):
                         det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
                         bbox_xywh = xyxy2xywh(det[:, :4]).cpu()
-                        dict_box = dict()
 
-                        '''
-                        将边界框的坐标从左上角和右下角表示(xyxy格式), 转换为中心点坐标、宽度和高度表示(xywh格式)
-                        这个步骤可以使用xyxy2xywh函数实现, det[:, :4]是一个形状为(N, 4)的numpy数组, 包含了N个边界框的坐标,
-                        第一列是左上角x坐标, 第二列是左上角y坐标, 第三列是右下角x坐标, 第四列是右下角y坐标
-                        '''
+                        confs = det[:, 4:5].cpu()
 
-                        confs = det[:, 4:5].cpu()  # 获取每个边界框的置信度得分
-
-                        # update()方法用于使用当前帧的最新检测结果更新跟踪器的状态，并输出当前跟踪的目标列表
                         outputs = deepsort.update(bbox_xywh, confs, im0)
                         print(outputs)
                         cmap = plt.get_cmap('tab20b')
                         colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
-
-                        '''
-                        outputs列表包含跟踪目标的信息, confs列表包含每个目标的置信度分数
-                        bbox_xywh是一个格式为[x, y, width, height]的边界框坐标列表
-                        confs是每个检测到的对象的置信度分数列表, im0是一个图像
-                        '''
 
                         if len(outputs) > 0:
                             for idx, track in enumerate(outputs):
@@ -332,22 +277,20 @@ class DeepsortThread(QThread):
 
                         prev_trackers = {track[-1]: track[:-1] for track in outputs}
 
-                    im0 = annotator.result()  # 返回的数据结构可能包括每个目标的ID、位置、速度、加速度等信息, 以及图像上绘制的边界框和标签等视觉化结果
+                    im0 = annotator.result()
                     self.send_img2.emit(im0)
 
         except Exception as e:
             traceback.print_exc()
 
 
-class Window(QWidget):  # 是所有用户界面类的基类，它能接收所有的鼠标、键盘和其他系统窗口事件
+class Window(QWidget):
 
     def __init__(self):
         super().__init__()
-        # 设置标题(Video Player)
         self.setWindowTitle("Player")
         self.button_Adaptive = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
-        # 从屏幕上(0, 0),位置开始(即为最左上角的点),显示一个1020*600的界面(长1020, 宽600)
         self.setGeometry(0, 0, 1280, 700)
 
         # 创建播放按钮
@@ -367,34 +310,30 @@ class Window(QWidget):  # 是所有用户界面类的基类，它能接收所有
         self.replayBtn.clicked.connect(self.replay)
 
         # comboBox自动搜索.pt文件
-        self.comboBox = QtWidgets.QComboBox()  # 创建comboBox
+        self.comboBox = QtWidgets.QComboBox()
         self.comboBox.clear()
-        self.pt_list = os.listdir('./pt')  # 搜索本地pt文件夹
-        self.pt_list = [file for file in self.pt_list if file.endswith('.pt')]  # 搜索本地pt文件
-        self.comboBox.addItems(self.pt_list)  # 添加pt文件
+        self.pt_list = os.listdir('./pt')
+        self.pt_list = [file for file in self.pt_list if file.endswith('.pt')]
+        self.comboBox.addItems(self.pt_list)
         self.qtimer_search = QTimer(self)
         self.qtimer_search.timeout.connect(lambda: self.search_pt)
         self.qtimer_search.start(2000)
         self.model_type = self.comboBox.currentText()
         self.comboBox.currentTextChanged.connect(self.change_model)  # 发送信号
 
-        # 创建选择文件按钮
         self.FileBtn = QPushButton('Select Video')
         self.FileBtn.clicked.connect(self.open_file)
 
-        # 创建label来放置左视频播放
         self.left_label = QLabel()
-        self.left_label.setFrameStyle(QFrame.Panel | QFrame.Sunken)  # 设置样式, 不然无法显示
+        self.left_label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
         self.left_label.setFixedWidth(640)
         self.left_label.setFixedHeight(640)
 
-        # 创建label来放置右视频播放
         self.right_label = QLabel()
-        self.right_label.setFrameStyle(QFrame.Panel | QFrame.Sunken)  # 设置样式, 不然无法显示
+        self.right_label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
         self.right_label.setFixedWidth(640)
         self.right_label.setFixedHeight(640)
 
-        # 创建分割线
         self.splitter = QSplitter(Qt.Horizontal)
         self.splitter.addWidget(self.left_label)
         self.splitter.addWidget(self.right_label)
@@ -466,13 +405,12 @@ class Window(QWidget):  # 是所有用户界面类的基类，它能接收所有
         self.model_type = self.comboBox.currentText()
         self.yolothread.weights = "./pt/%s" % self.model_type
 
-    def show_image(self, img_src, label):  # 在Qlabel上显示检测的图片
+    def show_image(self, img_src, label):
         try:
-            ih, iw, _ = img_src.shape  # 图片的宽和高
-            w = label.geometry().width()  # label的宽
-            h = label.geometry().height()  # label的高
+            ih, iw, _ = img_src.shape
+            w = label.geometry().width()
+            h = label.geometry().height()
 
-            # keep original aspect ratio 保持原始纵横比
             if iw / w > ih / h:
                 scal = w / iw
                 nw = w
@@ -493,13 +431,13 @@ class Window(QWidget):  # 是所有用户界面类的基类，它能接收所有
         except Exception as e:
             print(repr(e))
 
-    def show_image2(self, img_src, label):  # 在Qlabel上显示检测的图片
+    def show_image2(self, img_src, label):
         try:
-            ih, iw, _ = img_src.shape  # 图片的宽和高
-            w = label.geometry().width()  # label的宽
-            h = label.geometry().height()  # label的高
+            ih, iw, _ = img_src.shape
+            w = label.geometry().width()
+            h = label.geometry().height()
 
-            # keep original aspect ratio 保持原始纵横比
+            # keep original aspect ratio
             if iw / w > ih / h:
                 scal = w / iw
                 nw = w
@@ -551,14 +489,13 @@ class Window(QWidget):  # 是所有用户界面类的基类，它能接收所有
         self.yolothread.jump_out = True
         self.Deepsort_thread.jump_out = True
 
-        # 按钮激活和关闭
         self.replayBtn.setEnabled(False)
         self.pauseBtn.setEnabled(False)
         self.playBtn.setEnabled(True)
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)  # QApplication是PyQt的整个后台管理的关键
+    app = QApplication(sys.argv)
     window = Window()
     window.show()
     sys.exit(app.exec_())
