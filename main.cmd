994dcb5197e30dac436b2ac0064ae9f23086cf0a
Jiahao Guo <100775189+autJiahao@users.noreply.github.com>
2023-05-26 00:01:09 +1200

    Delete main.cmd

--- main.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
-set PATH=c:\users\lenovo\appdata\local\programs\python\python37;%PATH%
-set PYTHONHOME=C:\Users\LENOVO\AppData\Local\Programs\Python\Python37
-"%~dp0.\main.exe" %*
